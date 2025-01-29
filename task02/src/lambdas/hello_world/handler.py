c5d0d1b06ff683890a7a822c0492b8c941891887
Mamatkasym Mamarasul <mamatkasym.mu@gmail.com>
2025-01-29 14:39:08 +0600

    added task 2

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -8,14 +8,17 @@ class HelloWorld(AbstractLambda):
 
     def validate_request(self, event) -> dict:
         pass
-
+        
     def handle_request(self, event, context):
         """
         Explain incoming event here
         """
         # todo implement business logic
-        return {"statusCode": 200, "message": "Hello from Lambda"}
-
+        return {
+         "statusCode": 200,
+         "message": "Hello from Lambda"
+     }
+    
 
 HANDLER = HelloWorld()
 
