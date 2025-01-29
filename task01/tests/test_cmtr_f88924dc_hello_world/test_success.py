abaae37e139255ecf310430fb770943a0f72f598
Mamatkasym Mamarasul <mamatkasym.mu@gmail.com>
2025-01-29 12:05:23 +0600

    last commit

--- task01/tests/test_cmtr_f88924dc_hello_world/test_success.py
+++
@@ -1,8 +0,0 @@
-from tests.test_cmtr_f88924dc_hello_world import CmtrF88924dcHelloWorldLambdaTestCase
-
-
-class TestSuccess(CmtrF88924dcHelloWorldLambdaTestCase):
-
-    def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
-
