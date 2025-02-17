231cbd8563a9d35d72f91ac7b04c075f6ce77860
Mamatkasym Mamarasul <mamatkasym.mu@gmail.com>
2025-02-17 18:00:55 +0600

    added task 10

---
+++ task09/src/lambdas/api_handler/handler.py
@@ -0,0 +1,47 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+import requests
+
+_LOG = get_logger(__name__)
+
+
+class ApiHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        _LOG.info(f"EVENT: {event}")
+        params = {
+            "latitude": 52.54,
+            "longitude": 13.41,
+            "hourly": ["temperature_2m", "precipitation", "wind_speed_10m"],
+            "current": ["temperature_2m", "relative_humidity_2m"]
+        }
+
+        request_context = event.get("requestContext", {})
+        request = request_context.get("http", {})
+        method = request.get("method")
+        path = request.get("path")
+
+        if path == '/weather' and method == "GET":
+            response = requests.get("https://api.open-meteo.com/v1/forecast")
+
+            return response.json()
+
+        else:
+            return {
+                "statusCode": 400,
+                "message": f"Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+            }
+
+
+HANDLER = ApiHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
---
+++ task10/src/lambdas/processor/handler.py
@@ -0,0 +1,71 @@
+import json
+import os
+import uuid
+from decimal import Decimal
+
+import boto3
+import requests
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger(__name__)
+
+
+class Processor(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        _LOG.info(f"EVENT: {event}")
+        params = {
+            "latitude": 52.54,
+            "longitude": 13.41,
+            "hourly": ["temperature_2m", "precipitation", "wind_speed_10m"],
+            "current": ["temperature_2m", "relative_humidity_2m"]
+        }
+
+        response = requests.get(
+            "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+
+        data = response.json()
+        obj = {
+            "id": uuid.uuid4().hex,
+            "forecast": {
+                "elevation": data["elevation"],
+                "generationtime_ms": data["generationtime_ms"],
+                "hourly": {
+                    "temperature_2m": data["hourly"]["temperature_2m"],
+                    "time": data["hourly"]["time"]
+                },
+                "hourly_units": {
+                    "temperature_2m": data["hourly_units"]["temperature_2m"],
+                    "time": data["hourly_units"]["time"]
+                },
+                "latitude": data["latitude"],
+                "longitude": data["longitude"],
+                "timezone": data["timezone"],
+                "timezone_abbreviation": data["timezone_abbreviation"],
+                "utc_offset_seconds": data["utc_offset_seconds"]
+            }
+        }
+        obj = json.loads(json.dumps(obj), parse_float=Decimal)
+
+        dynamodb = boto3.resource('dynamodb', region_name=os.environ.get("region", "eu-central-1"))
+
+        table_name = os.environ["target_table"]
+
+        _LOG.info(f"Table: {table_name}")
+        table = dynamodb.Table(table_name)
+
+        table.put_item(Item=obj)
+
+
+HANDLER = Processor()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
