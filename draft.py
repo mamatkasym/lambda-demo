580657a03d2cba4c1b0f8123fd33c1aa51e9b3fb
Mamatkasym Mamarasul <mamatkasym.mu@gmail.com>
2025-03-03 18:25:30 +0600

    complete task 12

--- draft.py
+++ draft.py
@@ -1,20 +1,5 @@
 import json, decimal
+import pprint
 
-def decode_auth_token(auth_token: str):
-    """Decodes the auth token"""
-    try:
-        # remove "Bearer " from the token string.
-        auth_token = auth_token.replace("Bearer ", "")
-        # decode using secret key, will crash if not set.
-        print(auth_token)
-        return jwt.decode(jwt=auth_token, key=SECRET, algorithms=["HS256"])
-
-    except Exception:
-        _LOG.info("Invalid token. Please log in again.")
-        return
-
-d = {
-    "accessToken": "eyJraWQiOiJmbUoxdGdoZDczOFJ6VXZYeVRxMmRaWnNlV1RaT1p6UW9wVnNWYkFYZDVzPSIsImFsZyI6IlJTMjU2In0.eyJvcmlnaW5fanRpIjoiMDAyNWVkYmQtYzRiZi00ZTBiLTk4M2MtNDc0MjY2M2NiMTU5Iiwic3ViIjoiNzNlNGU4YTItODBmMS03MGEwLWEzZDAtYzY4Y2FhNGM2ZjFjIiwiYXVkIjoibm1zYnFoc2tiMG84bTkwN3QxanFzMjNmYyIsImV2ZW50X2lkIjoiMzc0MDA3MzMtYzFlMi00ZDVjLWFjN2EtYzNhZjBiMjhkZmFjIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE3NDA1NzI4OTgsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS1jZW50cmFsLTEuYW1hem9uYXdzLmNvbVwvZXUtY2VudHJhbC0xXzZ3ZnFScmFiNiIsImNvZ25pdG86dXNlcm5hbWUiOiJlbWFpbEBnbWFpbC5jb20iLCJleHAiOjE3NDA1NzY0OTcsImlhdCI6MTc0MDU3Mjg5OCwianRpIjoiODA4OGYzMTQtOGNjZC00YjQxLTk4NTQtNjFjZGI1Y2Y1YTZkIn0.waFH-JmSNVmO2zVNdBsYmEYwaF9HKPkhw6-n4VGUoXEc0_yZxFrDgFIdThielgP0tnvxQormsDg12UNEXEM3VQK67VWRD5cCnZKgf3zwpxkteh9qDUxhklKU3C-VpYRATucmYZUdXAvgExu4k7CLVgQou5vhhWa5AwfOai2EY8Xkqm6ME4wBn6XPsJc5AwsfK4YjHfWSfrr1hJ1WbftXS4wLN5exWRiDbMz_o9xCMg6FFPJfZ0tGKhWSNocBryR9H239sOHKIe8hpDoxfZOHDV2WAFsBMrXy9Wcl6XnBh-0lA-jwc-y-0xvvQmxqvJhBYIkPMWvEXDJheeLeTM4qJQ"
-}
-
-print(d["accessToken"])
+d = {"name": "Games-GQL-API", "resource_type": "appsync", "primary_auth_type": "API_KEY", "api_key_expiration_days": 7, "schema_path": "schema.graphql", "data_sources": [{"name": "gamesDB", "type": "AMAZON_DYNAMODB", "service_role_name": "appsync_role", "dynamodb_config": {"table_name": "GamesDB", "aws_region": "eu-central-1"}}], "resolvers": [{"kind": "UNIT", "type_name": "Mutation", "field_name": "addGame", "data_source_name": "gamesDB", "runtime": "VTL", "request_mapping_template_path": "resolvers/mutation/addgame/req_mapping_template.vtl", "response_mapping_template_path": "resolvers/mutation/addgame/resp_mapping_template.vtl"}, {"kind": "UNIT", "type_name": "Query", "field_name": "showGame", "data_source_name": "gamesDB", "runtime": "JS", "code_path": "resolvers/query/showgame/code.js"}, {"kind": "UNIT", "type_name": "Query", "field_name": "showPlayer", "data_source_name": "gamesDB", "runtime": "JS", "code_path": "resolvers/query/showplayer/code.js"}, {"kind": "PIPELINE", "type_name": "Mutation", "field_name": "addPlayer", "runtime": "VTL", "pipeline_config": {"functions": ["getGameItem", "addPlayer"]}, "request_mapping_template_path": "resolvers/mutation/addplayer/req_mapping_template.vtl", "response_mapping_template_path": "resolvers/mutation/addplayer/resp_mapping_template.vtl"}], "functions": [{"name": "getGameItem", "description": "", "data_source_name": "gamesDB", "runtime": "VTL", "function_version": "2018-05-29", "request_mapping_template_path": "functions/getgameitem/req_mapping_template.vtl", "response_mapping_template_path": "functions/getgameitem/resp_mapping_template.vtl"}, {"name": "addPlayer", "description": "", "data_source_name": "gamesDB", "runtime": "JS", "code_path": "functions/addplayer/code.js"}], "log_config": {"logging_enabled": True, "field_log_level": "ERROR", "cloud_watch_logs_role_name": "appsync_role", "exclude_verbose_content": False}, "tags": {}, "deployment_package": "appsync_Games-GQL-API.zip"}
+print(print(json.dumps(d, indent=4)))
